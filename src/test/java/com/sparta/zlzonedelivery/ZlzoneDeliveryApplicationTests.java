8ec33c54ce53d45954f82f574d1a9f5bfcaff3d5
Choi Seongwon <sw.choi@khu.ac.kr>
2024-08-23 14:15:44 +0900

    init commit

---
+++ src/test/java/com/sparta/zlzonedelivery/ZlzoneDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.zlzonedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlzoneDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
