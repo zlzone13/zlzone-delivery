a9e14f10bbee65cf91ba5a5a14299a7e29b77f10
asd42270 <lyb991113@gmail.com>
2024-08-26 12:39:10 +0900

    [Feat] Store 엔티티를 생성한다. - user, categotry 엔티티 생성 시 연관관계 추가

---
+++ src/main/java/com/sparta/zlzonedelivery/store/entity/Store.java
@@ -0,0 +1,50 @@
+package com.sparta.zlzonedelivery.store.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_stores")
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    //user_id
+
+    @Column(name = "store_name")
+    private String storeName;
+
+    @Column
+    private String description;
+
+    @Column
+    private String announcement;
+
+    @Column(name = "b_no")
+    private String bNo;
+
+    @Column(name = "telephone_no")
+    private String telephoneNo;
+
+    @Column(name = "delivery_area")
+    private String deliveryArea;
+
+    @Column(name = "open_close_time")
+    private String openCloseTime;
+
+    @Column(name = "country_info")
+    private String countryInfo;
+
+}
+
