1ec038f2c4a50bea09c699827bb4b8d52054b320
asd42270 <lyb991113@gmail.com>
2024-08-26 16:56:33 +0900

    [feat] CreateRequestDto에 Validation을 적용한다. - 의존성 추가 - StoreCreateRequestDto: 유효성 검사 적용

--- src/main/java/com/sparta/zlzonedelivery/store/service/dtos/StoreCreateRequestDto.java
+++ src/main/java/com/sparta/zlzonedelivery/store/service/dtos/StoreCreateRequestDto.java
@@ -1,14 +1,20 @@
 package com.sparta.zlzonedelivery.store.service.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record StoreCreateRequestDto(
         //category
+        @NotBlank
         String storeName,
         String description,
         String announcement,
+        @NotBlank
         String bNo,
         String telephoneNo,
         String deliveryArea,
         String openCloseTime,
+        @NotBlank
         String countryInfo
 ) {
 
