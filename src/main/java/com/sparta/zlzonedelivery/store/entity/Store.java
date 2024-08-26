dde26aab8b013b3337ffdaa23d2504f6d482210a
asd42270 <110414025+asd42270@users.noreply.github.com>
2024-08-26 15:19:26 +0900

    feat: Store 엔티티 생성 (#12)
    * feat: Redis 의존성 추가 (#2)
    
    * [Feat] 로컬 설정을 위해 application.yml을 생성한다.
    - redis 기본 설정
    - h2데이터베이스 기본 설정(PostgreSQL 사용)
    - jpa 설정
    - profile local로 설정하여 local에서만 동작하도록 설정
    
    * [Feat] Store 엔티티를 생성한다.
    - user, categotry 엔티티 생성 시 연관관계 추가
    
    * [feat] 공통 컬럼을 생성한다.- JPA Auditing
    - BaseTimeEntity: auditing 구현 생성, 수정일, 사용자 추가
    - Store: category_id 주석 추가
    
    * [feat] 공통 컬럼을 생성한다.- 논리 삭제 테이블
    - SoftDeletableBaseTimeEntity: 논리 삭제 컬럼을 구현, BaseTimeEntity를 상속받는 클래스
    - ZlzoneDeliveryApplication: @EnableJpaAuditing 어노테이션 추가
    
    * [feat] Store 엔티티를 생성한다.
    - 논리 삭제 테이블을 상속받는다.
    
    * [feat] Store 엔티티를 생성한다.
    - ZlzoneDeliveryApplication: Auditing 어노테이션 삭제
    
    * feat: 유저 테이블 생성, Base Entity 생성 (#9)
    
    * [feat] Store 엔티티를 생성한다.
    - BaseTimeEntity, SoftDeletableBaseTimeEntity 삭제
    - Store 클래스 상속 테이블 변경
    
    * [feat] Store 엔티티를 생성한다.
    - @SQLDelete 어노테이션 추가
    - 컬럼별 제한 길이 추가
    
    * [feat] Store 엔티티를 생성한다.
    - User 연관관계 추가
    
    ---------
    
    Co-authored-by: upRightLee <117060896+lswoo0705@users.noreply.github.com>
    Co-authored-by: Seongwon Choi <sw.choi@khu.ac.kr>

---
+++ src/main/java/com/sparta/zlzonedelivery/store/entity/Store.java
@@ -0,0 +1,63 @@
+package com.sparta.zlzonedelivery.store.entity;
+
+import com.sparta.zlzonedelivery.global.entity.BaseEntity;
+import com.sparta.zlzonedelivery.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_stores")
+@SQLDelete(sql = "UPDATE p_stores SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    //user_id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    //category_id
+
+    //store_location_id
+
+    @Column(name = "store_name", length = 100)
+    private String storeName;
+
+    @Column
+    private String description;
+
+    @Column
+    private String announcement;
+
+    @Column(name = "b_no", length = 20)
+    private String bNo;
+
+    @Column(name = "telephone_no", length = 20)
+    private String telephoneNo;
+
+    @Column(name = "delivery_area", length = 100)
+    private String deliveryArea;
+
+    @Column(name = "open_close_time")
+    private String openCloseTime;
+
+    @Column(name = "country_info")
+    private String countryInfo;
+
+}
+
