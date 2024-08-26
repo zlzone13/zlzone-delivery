9fdfb647c87b4588c4e0d324f569c219b30e5752
asd42270 <lyb991113@gmail.com>
2024-08-26 15:23:05 +0900

    [feat] Store Repository를 생성한다.

---
+++ src/main/java/com/sparta/zlzonedelivery/store/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.zlzonedelivery.store.repository;
+
+import com.sparta.zlzonedelivery.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+}
