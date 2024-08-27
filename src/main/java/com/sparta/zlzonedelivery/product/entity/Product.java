60f8d3096430e3cf95a4e9ad74806d9b1e05cb59
asd42270 <lyb991113@gmail.com>
2024-08-27 19:26:01 +0900

    [feat] Product: Store FetchType 설정

--- src/main/java/com/sparta/zlzonedelivery/product/entity/Product.java
+++ src/main/java/com/sparta/zlzonedelivery/product/entity/Product.java
@@ -4,6 +4,7 @@ import com.sparta.zlzonedelivery.global.entity.BaseEntity;
 import com.sparta.zlzonedelivery.store.entity.Store;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,7 +39,7 @@ public class Product extends BaseEntity {
     @Column(name = "is_shown")
     private Boolean isShown;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "store_id")
     private Store store;
 
