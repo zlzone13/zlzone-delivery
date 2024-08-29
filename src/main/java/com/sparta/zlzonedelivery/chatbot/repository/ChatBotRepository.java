79bdb16fcdb582dd32b56c44797b8a6de8097751
asd42270 <lyb991113@gmail.com>
2024-08-29 20:00:54 +0900

    feat: ChatBot 조회 기능 구현 - ChatBotService: 단건 조회 기능 로직 작성 - ErrorCode: 에러코드 추가 - ChatBotRepository: 메소드 추가

--- src/main/java/com/sparta/zlzonedelivery/chatbot/repository/ChatBotRepository.java
+++ src/main/java/com/sparta/zlzonedelivery/chatbot/repository/ChatBotRepository.java
@@ -3,8 +3,11 @@ package com.sparta.zlzonedelivery.chatbot.repository;
 import com.sparta.zlzonedelivery.chatbot.entity.ChatBot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ChatBotRepository extends JpaRepository<ChatBot, UUID> {
+
+    Optional<ChatBot> findByIdAndIsPublicIsTrue(UUID id);
 
 }
--- src/main/java/com/sparta/zlzonedelivery/chatbot/service/ChatBotService.java
+++ src/main/java/com/sparta/zlzonedelivery/chatbot/service/ChatBotService.java
@@ -3,6 +3,7 @@ package com.sparta.zlzonedelivery.chatbot.service;
 
 import com.sparta.zlzonedelivery.chatbot.entity.ChatBot;
 import com.sparta.zlzonedelivery.chatbot.repository.ChatBotRepository;
+import com.sparta.zlzonedelivery.chatbot.service.dtos.ChatBotReadResponseDto;
 import com.sparta.zlzonedelivery.chatbot.service.dtos.ChatBotCreateResponseDto;
 import com.sparta.zlzonedelivery.chatbot.service.dtos.ChatBotServiceCreateDto;
 import com.sparta.zlzonedelivery.global.error.CustomException;
@@ -11,6 +12,8 @@ import com.sparta.zlzonedelivery.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @Transactional(readOnly = true)
@@ -32,6 +35,12 @@ public class ChatBotService {
                 chatBotServiceCreateDto.response(), chatBotServiceCreateDto.user());
 
         return ChatBotCreateResponseDto.fromEntity(chatBotRepository.save(chatBot));
+    }
+
+    public ChatBotReadResponseDto getQueryAndAnswer(UUID chatbotId) {
+
+        return  ChatBotReadResponseDto.fromEntity(chatBotRepository.findByIdAndIsPublicIsTrue(chatbotId)
+                .orElseThrow(()->new CustomException(ErrorCode.QUERY_AND_ANSWER_NOT_FOUND)));
     }
 
 }
