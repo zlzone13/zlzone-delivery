112a006a173ab3d1d4a7609de32ee038be06749c
asd42270 <lyb991113@gmail.com>
2024-08-29 19:44:22 +0900

    feat: AI 호출 및 ChatBot 생성 기능 - ChatBotController: 컨트롤러 코드 작성, 권한 처리 코드 작성 - UserDetailsImpl: User 정보를 가져오도록 필드에 Getter 추가

--- src/main/java/com/sparta/zlzonedelivery/chatbot/controller/ChatBotController.java
+++ src/main/java/com/sparta/zlzonedelivery/chatbot/controller/ChatBotController.java
@@ -1,7 +1,18 @@
 package com.sparta.zlzonedelivery.chatbot.controller;
 
 import com.sparta.zlzonedelivery.chatbot.service.ChatBotService;
+import com.sparta.zlzonedelivery.chatbot.service.dtos.ChatBotCreateRequestDto;
+import com.sparta.zlzonedelivery.chatbot.service.dtos.ChatBotCreateResponseDto;
+import com.sparta.zlzonedelivery.chatbot.service.dtos.ServiceCreateDto;
+import com.sparta.zlzonedelivery.global.auth.security.UserDetailsImpl;
+import com.sparta.zlzonedelivery.global.error.CustomException;
+import com.sparta.zlzonedelivery.global.error.ErrorCode;
+import com.sparta.zlzonedelivery.user.UserRole;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +22,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatBotController {
 
     private final ChatBotService chatBotService;
+
+    private final OpenAiChatModel openAiChatModel;
+
+    @PostMapping
+    public ChatBotCreateResponseDto createAnswer(@RequestBody ChatBotCreateRequestDto requestDto,
+                                                 @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        UserRole role = userDetails.getRole();
+
+        if (role.getValue().equals("CUSTOMER")) {
+            throw new CustomException(ErrorCode.ACCESS_DENIED);
+        }
+
+        String response = openAiChatModel.call(requestDto.query());
+        ServiceCreateDto serviceCreateDto = new ServiceCreateDto(requestDto.query(), response, userDetails.getUser());
+
+        return chatBotService.createAnswer(serviceCreateDto);
+    }
+
 
 }
