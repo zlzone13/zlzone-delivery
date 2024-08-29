c5cd28300019f20a589dc91ae56200c7525b08c0
asd42270 <lyb991113@gmail.com>
2024-08-29 20:02:02 +0900

    feat: ChatBot 조회 기능 구현 - ChatBotController: 사용자 검증 로직 메서드로 분리, 단건 조회 메서드 작성

--- src/main/java/com/sparta/zlzonedelivery/chatbot/controller/ChatBotController.java
+++ src/main/java/com/sparta/zlzonedelivery/chatbot/controller/ChatBotController.java
@@ -3,6 +3,7 @@ package com.sparta.zlzonedelivery.chatbot.controller;
 import com.sparta.zlzonedelivery.chatbot.service.ChatBotService;
 import com.sparta.zlzonedelivery.chatbot.service.dtos.ChatBotCreateRequestDto;
 import com.sparta.zlzonedelivery.chatbot.service.dtos.ChatBotCreateResponseDto;
+import com.sparta.zlzonedelivery.chatbot.service.dtos.ChatBotReadResponseDto;
 import com.sparta.zlzonedelivery.chatbot.service.dtos.ChatBotServiceCreateDto;
 import com.sparta.zlzonedelivery.global.auth.security.UserDetailsImpl;
 import com.sparta.zlzonedelivery.global.error.CustomException;
@@ -11,10 +12,14 @@ import com.sparta.zlzonedelivery.user.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/chat-bots")
@@ -29,16 +34,27 @@ public class ChatBotController {
     public ChatBotCreateResponseDto createAnswer(@RequestBody ChatBotCreateRequestDto requestDto,
                                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        UserRole role = userDetails.getRole();
-
-        if (role.getValue().equals("CUSTOMER")) {
-            throw new CustomException(ErrorCode.ACCESS_DENIED);
-        }
+        validateUser(userDetails);
 
         String response = openAiChatModel.call(requestDto.query());
         ChatBotServiceCreateDto chatBotServiceCreateDto = new ChatBotServiceCreateDto(requestDto.query(), response, userDetails.getUser());
 
         return chatBotService.createAnswer(chatBotServiceCreateDto);
+    }
+
+    @GetMapping("/{chat-bot_id}")
+    public ChatBotReadResponseDto getQueryAndAnswer(@PathVariable("chat-bot_id") UUID uuid,
+                                                    @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        validateUser(userDetails);
+        return chatBotService.getQueryAndAnswer(uuid);
+    }
+
+    private static void validateUser(UserDetailsImpl userDetails) {
+        UserRole role = userDetails.getRole();
+
+        if (role.getValue().equals("CUSTOMER")) {
+            throw new CustomException(ErrorCode.ACCESS_DENIED);
+        }
     }
 
 
