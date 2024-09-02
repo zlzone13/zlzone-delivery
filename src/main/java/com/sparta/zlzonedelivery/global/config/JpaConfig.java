ef2c94f8cd128222f3404e16e9f6d0a714422b1a
Seongwon Choi <vlflalektzja@naver.com>
2024-09-02 17:13:39 +0900

    feat: User 도메인 CRUD, 테스트 작성 완료 (#34)

--- src/main/java/com/sparta/zlzonedelivery/global/config/JpaConfig.java
+++ src/main/java/com/sparta/zlzonedelivery/global/config/JpaConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class JpaConfig {
 
 }
--- src/main/java/com/sparta/zlzonedelivery/user/controller/dto/UserSearchResponseDto.java
+++ src/main/java/com/sparta/zlzonedelivery/user/controller/dto/UserSearchResponseDto.java
@@ -1,5 +1,40 @@
 package com.sparta.zlzonedelivery.user.controller.dto;
 
-public record UserSearchResponseDto() {
+import com.sparta.zlzonedelivery.user.User;
+import com.sparta.zlzonedelivery.user.UserRole;
+
+import java.time.LocalDateTime;
+
+public record UserSearchResponseDto(
+        Long id,
+        String username,
+        String nickname,
+        String email,
+        UserRole role,
+        Boolean isPublic,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime updatedAt,
+        String updatedBy,
+        LocalDateTime deletedAt,
+        String deletedBy
+) {
+
+    public UserSearchResponseDto(User user) {
+        this(
+                user.getId(),
+                user.getUsername(),
+                user.getNickname(),
+                user.getEmail(),
+                user.getRole(),
+                user.getIsPublic(),
+                user.getCreatedAt(),
+                user.getCreatedBy(),
+                user.getUpdatedAt(),
+                user.getUpdatedBy(),
+                user.getDeletedAt(),
+                user.getDeletedBy()
+        );
+    }
 
 }
